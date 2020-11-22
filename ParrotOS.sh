d3c97bd6242765b326581d047dc85f248d1ec018
TheDarkRoot <37486414+TheDarkRoot@users.noreply.github.com>
2020-11-23 01:09:14 +0300

    Add files via upload

---
+++ ParrotOS.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+spin () {
+local pid=$!
+local delay=0.10
+local spinner=( '\033[34;1m■■■■■■■' '\033[32;1m█\033[33;1m■■■■■■' '\033[33;1m■\033[32;1m█\033[33;1m■■■■■' '\033[33;1m■■\033[32;1m█\033[33;1m■■■■' '\033[33;1m■■■\033[32;1m█\033[33;1m■■■' '\033[33;1m■■■■\033[32;1m█\033[33;1m■■' '\033[33;1m■■■■■\033[32;1m█\033[33;1m■' '\033[33;1m■■■■■■\033[32;1m█' '\033[34;1m■■■■■■■' '\033[33;1m■■■■■■\033[32;1m█' '\033[33;1m■■■■■\033[32;1m█\033[33;1m■' '\033[33;1m■■■■\033[32;1m█\033[33;1m■■' '\033[33;1m■■■\033[32;1m█\033[33;1m■■■' '\033[33;1m■■\033[32;1m█\033[33;1m■■■■' '\033[33;1m■\033[32;1m█\033[33;1m■■■■■' '\033[32;1m█\033[33;1m■■■■■■' )
+
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+for i in "${spinner[@]}"
+do
+  echo -ne "\r$CC [$YY↓$CC]$YY Downloading please wait...$CC 【$i$CC】";
+  sleep $delay
+  printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+printf "$WW⟫$GG Completed.\n";
+echo "";
+}
+#Colors
+BB="\033[34;1m" # Blue Light
+YY="\033[33;1m" # Yellow Light
+GG="\033[32;1m" # Green Light
+WW="\033[0;1m"  # White Light
+RR="\033[31;1m" # Red Light
+CC="\033[36;1m" # Cyan Light
+MM="\033[35;1m" # Magenta Light
+B="\033[34;1m"  # Blue
+Y="\033[33;1m"  # Yellow
+G="\033[32;1m"  # Green
+W="\033[0;1m"   # White
+R="\033[31;1m"  # Red
+C="\033[36;1m"  # Cyan
+M="\033[35;1m"  # Magenta
+#Terpack Banner
+echo -e "\n$CC #######$YY ##################$CC #######$YY ####################
+$CC    #    #####  #####          #     ####   ####  #
+$CC    #    #    # #    #         #    #    # #    # #
+$CC    #    #    # #    #  #####  #    #    # #    # #
+$CC    #    #    # #####          #    #    # #    # #
+$CC    #    #    # #   #          #    #    # #    # #
+$CC    #    #####  #    #         #     ####   ####  ######
+$YY ####################[$GG TheDarkRoot$YY ]####################\n
+$GG 0{======================$WW INFO $GG=======================}0
+$GG |$YY [$CC=$YY]$WW Name     $CC:$WW ParrotOS-T$GG                           |
+$GG |$YY [$CC=$YY]$WW Code     $CC:$WW Shell$GG                                |
+$GG |$YY [$CC=$YY]$WW Version  $CC:$WW v1.0.0 (Alpha)$GG                       |
+$GG |$YY [$CC=$YY]$WW Author   $CC:$WW TheDarkRoot$GG                          |
+$GG |$YY [$CC=$YY]$WW Email    $CC:$WW 7H3D4RKR007@gmail.com$GG                |
+$GG |$YY [$CC=$YY]$WW Github   $CC:$WW https://github.com/TheDarkRoot$GG       |
+$GG |$YY [$CC=$YY]$WW Telegram $CC:$WW @TheDarkRoot (t.me/TheDarkRoot)$GG      |
+$GG |$YY [$CC=$YY]$WW Team     $CC:$WW TurkHackTeam (www.turkhackteam.org)$GG  |
+$GG 0{===================================================}0\n"
+#Termux Key Update
+echo -e "$CC [$YY*$CC]$GG ParrotOS theme updating...";
+( cd /data/data/com.termux/files/usr/etc/;curl https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Software%20Files/ParrotOS.termux -o bash.bashrc;cd ~/;termux-reload-settings; ) &> /dev/null & spin;
+echo -e "$CC [$YY*$CC]$GG Update successful.\n"
---
+++ Readme.md
@@ -0,0 +1,39 @@
+<p align="center"><a href="https://turkhackteam.org"><img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Banner.png" width="250"></a></p>
+<h2 align="center"><b>ParrotOS-T v1.0.0 (Alpha)</b></h2>
+<h4 align="center">Parrot OS theme for Termux.</h4>
+</p>
+<p align="center"><a href="center"><a href="https://t.me/TheDarkRoot"><img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Telegram.png" width="30"></a>     <a href="center"><a href="https://instagram.com/TheDarkRoot"><img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Instagram.png" width="30"></a>     <a href="center"><a href="https://twitter.com/TDarkRoot"><img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Twitter.png" width="30"></a>     <a href="https://github.com/karjok/terkey"><img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Github.png" width="30"></a></p>
+</p>
+<hr>
+<p align="center"><a href="#Description">Description</a> &bull; <a href="#Features">Features</a> &bull; <a href="#Installation">Installation</a> &bull; <a href="#Screenshot">Screenshot</a></p>
+<hr>
+
+
+# Description
+
+- Parrot OS theme for Termux.
+- Developer of this software: **[@TheDarkRoot](https://github.com/TheDarkRoot)**
+- Official social media accounts: [@Telegram](https://t.me/TheDarkRoot) &bull; [@Instagram](https://instagram.com/TheDarkRoot) &bull; [@Twitter](https://twitter.com/TDarkRoot) &bull; [@Github](https://github.com/TheDarkRoot)
+
+> ***Warning: This project is created only for educational purposes and cannot be used for law violation or personal gain.
+The author of this project is not responsible for any possible harm caused by the materials of this project.***
+
+# Features
+
+- [x] Supports Android platform.
+
+# Installation
+
+> ***Open the terminal and type following commands.***
+```
+$ pkg install git
+$ git clone https://github.com/TheDarkRoot/ParrotOS-T.git
+$ cd ParrotOS-T
+$ chmod +x ParrotOS-T.sh
+$ bash ParrotOS-T.sh
+```
+
+# Screenshot
+
+[<img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Screenshots/ParrotOS%2001.png" width=175>](https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Screenshots/ParrotOS%2001.png)
+[<img src="https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Screenshots/ParrotOS%2002.png" width=250>](https://raw.githubusercontent.com/TheDarkRoot/FileStore/master/Images/TheDarkRoot/Screenshots/ParrotOS%2002.png)
